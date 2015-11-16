ed2d31cde0ad77fcd696966f9fba1884570916cd
Julien Blanchard <julien@sideburns.eu>
2015-11-16 18:44:18 +0100

    Log some stuff

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ use regex::Regex;
 use std::env;
 
 fn main() {
+    println!("Starting email-checker...");
+
     let re = Regex::new(r"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$").unwrap();
 
     match env::args().nth(1) {
-        Some(date) => {
-            if re.is_match(&date) {
-                println!("true");
+        Some(email) => {
+            if re.is_match(&email) {
+                println!("{} is a valid email.", email);
             } else {
-                println!("false");
+                println!("{} is NOT a valid email.", email);
             }
         }
         None => {
